1d305e9545531d483dfb42092b49b470353214e9
sakibsarkar <sakibsarkar707@gmail.com>
2023-08-20 18:18:01 +0600

    function repeat bug fixed.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let serial = 0;
+let item_serial_number = 0;
 let item_total = 0;
 function addItems(thisCard) {
     document.querySelector('.purchase').disabled = "true"
-    serial++;
+    item_serial_number++;
     // variables
     let item_list = document.querySelector(".item_list");
     let total_price = document.querySelector("#total_price");
@@ -12,7 +12,7 @@ function addItems(thisCard) {
     // main logic start
     let item_name = thisCard.childNodes[3].childNodes[3].innerText;
     let per_item_price = parseFloat(thisCard.childNodes[3].childNodes[5].childNodes[0].innerText);
-    
+
     item_total = item_total + per_item_price
     if (item_total > 0) {
         document.querySelector(".purchase").classList.remove("disabled")
@@ -27,45 +27,38 @@ function addItems(thisCard) {
     total_price.innerText = item_total + ".00"
     discount_total_price.innerText = total_price.innerText
     let ul = document.createElement("ul");
-    ul.innerText = serial + ". " + item_name;
+    ul.innerText = item_serial_number + ". " + item_name;
     item_list.appendChild(ul);
 
-    document.querySelector(".apply").addEventListener("click",function(){
-        let coupon_code = document.getElementById("coupon").value;
-        if(coupon_code == "SELL200"){
-            let my_discount = item_total * (20/100).toFixed(2)
-            if(my_discount % 1 == 0){
-                discount_price.innerText = my_discount  + ".00"
-                discount_total_price.innerText = item_total - parseFloat(discount_price.innerText)
-            }
-            else{
-                discount_price.innerText = my_discount.toFixed(2)
-                discount_total_price.innerText = item_total - parseFloat(discount_price.innerText)
-            }
+    return item_total
+
+}
+
+function couponApply() {
+    let coupon_code = document.getElementById("coupon").value;
+    if (coupon_code == "SELL200") {
+        console.log("fwasjf")
+        let my_discount = item_total * (20 / 100).toFixed(2)
+        if (my_discount % 1 == 0) {
+            discount_price.innerText = my_discount + ".00"
+            discount_total_price.innerText = item_total - parseFloat(discount_price.innerText)
         }
-    })
-}
-
-
-// coupon function
-
-function coupon() {
-
-    let total_price = document.querySelector("#total_price");
-    let discount_price = document.querySelector("#discount_price");
-    let discount_total_price = document.querySelector("#discount_total_price");
-    
-    
-    if(coupon_code == "SELL200"){
-        discount_price.innerText = (parseFloat(total_price.innerText) * (20/100))
-        discount_total_price.innerText = (parseFloat(total_price.innerText) - parseFloat(discount_price.innerText));
-         
+        else {
+            discount_price.innerText = my_discount.toFixed(2)
+            discount_total_price.innerText = item_total - parseFloat(discount_price.innerText)
+        }
     }
-    
+
+    else {
+        alert("Wrong coupon code");
+        document.getElementById("coupon").value = ""
+    }
+
 }
+
 
 // modal close
-document.querySelector(".myBTN").addEventListener("click",function(){
+document.querySelector(".myBTN").addEventListener("click", function () {
     let item_list = document.querySelector(".item_list");
     let total_price = document.querySelector("#total_price");
     let discount_total_price = document.querySelector("#discount_total_price");
